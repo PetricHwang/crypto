672ff8f3fd900cb5647b2e206e795c2232e11cf3
usernme <jiapeng.huang@mobeid.com>
2019-04-17 12:20:54 +0800

    DESede/CBC/PKCS7Padding

--- src/main/java/com/ppetric/des/DESedeUtils.java
+++ src/main/java/com/ppetric/des/DESedeUtils.java
@@ -4,12 +4,15 @@ import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.Hex;
 import org.bouncycastle.crypto.BufferedBlockCipher;
+import org.bouncycastle.crypto.CipherParameters;
 import org.bouncycastle.crypto.InvalidCipherTextException;
 import org.bouncycastle.crypto.KeyGenerationParameters;
 import org.bouncycastle.crypto.engines.DESedeEngine;
 import org.bouncycastle.crypto.generators.DESedeKeyGenerator;
+import org.bouncycastle.crypto.modes.CBCBlockCipher;
 import org.bouncycastle.crypto.paddings.PaddedBufferedBlockCipher;
 import org.bouncycastle.crypto.params.DESedeParameters;
+import org.bouncycastle.crypto.params.ParametersWithIV;
 
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
@@ -37,7 +40,7 @@ public class DESedeUtils {
     }
 
     /**
-     * 加密、解密
+     * 加密、解密(DESede/ECB/PKCS7Padding)
      *
      * @param encrypt
      * @param key
@@ -49,6 +52,33 @@ public class DESedeUtils {
         DESedeEngine engine = new DESedeEngine();
         BufferedBlockCipher cipher = new PaddedBufferedBlockCipher(engine);
         cipher.init(encrypt, new DESedeParameters(key));
+        byte[] out = new byte[cipher.getOutputSize(data.length)];
+        int len = cipher.processBytes(data, 0, data.length, out, 0);
+        try {
+            cipher.doFinal(out, len);
+        } catch (InvalidCipherTextException e) {
+            // TODO add log
+        }
+        return out;
+    }
+
+    /**
+     * 加密、解密(DESede/CBC/PKCS7Padding)
+     *
+     * @param encrypt
+     * @param key
+     * @param iv
+     * @param data
+     * @return
+     */
+    public static byte[] cryptCBC(boolean encrypt, byte[] key, byte[] iv, byte[] data) {
+
+        iv = iv == null ? new byte[8] : iv;
+        DESedeEngine engine = new DESedeEngine();
+        BufferedBlockCipher cipher = new PaddedBufferedBlockCipher(new CBCBlockCipher(engine));
+        CipherParameters params = new ParametersWithIV(new DESedeParameters(key), iv);
+        cipher.init(encrypt, params);
+
         byte[] out = new byte[cipher.getOutputSize(data.length)];
         int len = cipher.processBytes(data, 0, data.length, out, 0);
         try {
@@ -72,6 +102,16 @@ public class DESedeUtils {
 
         byte[] decrypt = cryptECB(false, pass, encrypt);
         System.out.println(new String(decrypt, StandardCharsets.UTF_8).replaceAll("\\u0000+$", ""));
+
+
+        String keyHex = "9DEFDC769999625AA5961E2D91E09999";
+        byte[] iv = Hex.decodeHex("0000000000000000");
+
+
+        byte[] encResult = cryptCBC(true, Hex.decodeHex(keyHex), iv, "1".getBytes());
+        System.out.println(Base64.encodeBase64String(encResult)); // RWvqowLoz78=
+        byte[] decResult = cryptCBC(false, Hex.decodeHex(keyHex), iv, encResult);
+        System.out.println(new String(decResult)); // 1
     }
 
 }
